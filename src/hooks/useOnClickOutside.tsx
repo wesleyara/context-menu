cf1373ba9fc576a257c5e54e4d0dac417e3304da
wesleyara <wes1gom@gmail.com>
2024-04-25 21:22:05 -0300

    feat: create hook

---
+++ src/hooks/useOnClickOutside.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+export const useOnClickOutside = (
+  ref: React.RefObject<any>,
+  handler: () => void,
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        handler();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, handler]);
+};
